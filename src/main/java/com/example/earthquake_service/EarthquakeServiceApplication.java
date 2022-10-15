3284d9453420212809a59fb0e12f2803801397fb
ilkeratik <ilkeratik35@gmail.com>
2022-10-15 13:01:00 +0300

    Initial commit

---
+++ src/main/java/com/example/earthquake_service/EarthquakeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.earthquake_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarthquakeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/earthquake_service/EarthquakeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.earthquake_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthquakeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
